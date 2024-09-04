5f976ac9416b285c26cc7b3e136b33dcf91671ee
21130875 <alu.21130875@correo.itlalaguna.edu.mx>
2024-09-04 16:01:13 -0600

    Agregue el form y empece con los botones para las funciones que lleva

---
+++ src/Package/Form1.java
@@ -0,0 +1,32 @@
+package Package;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Form1 {
+    private JTextField TXNum2;
+    private JTextField TFNum1;
+    private JButton BTNSuma;
+    private JButton BTNDiv;
+    private JButton BTNMult;
+    private JButton BTNResta;
+    private JTextField TXRes;
+    private JButton borrarButton;
+
+    public Form1(){
+
+
+        BTNSuma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               double num1 = Integer.parseInt(TFNum1.getText());
+               double num2 = Integer.parseInt(TXNum2.getText());
+               JOptionPane.showInputDialog(null, "La Respuesta es: ");
+            }
+        });
+    }
+
+}
+
+
