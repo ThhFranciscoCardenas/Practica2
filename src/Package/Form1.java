cafc6d6c3d724ab13159491b7d89ba59a5784c54
21130875 <alu.21130875@correo.itlalaguna.edu.mx>
2024-09-05 19:00:00 -0600

    Mejore el diseno y termine de agregar las funciones de los botones

--- src/Package/Form1.java
+++ src/Package/Form1.java
@@ -12,7 +12,6 @@ public class Form1 {
     private JButton BTNDiv;
     private JButton BTNMult;
     private JButton BTNResta;
-    private JTextField TXRes;
     private JButton borrarButton;
 
     public Form1(){
@@ -23,13 +22,54 @@ public class Form1 {
             public void actionPerformed(ActionEvent e) {
                double num1 = Integer.parseInt(TFNum1.getText());
                double num2 = Integer.parseInt(TXNum2.getText());
-               JOptionPane.showInputDialog(null, "La Respuesta es: ");
+
+               JOptionPane.showInputDialog(null, "La Respuesta es: "+ (num1+num2));
+            }
+        });
+        BTNResta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Integer.parseInt(TFNum1.getText());
+                double num2 = Integer.parseInt(TXNum2.getText());
+
+                JOptionPane.showInputDialog(null, "La Respuesta es: "+ (num1-num2));
+
+            }
+        });
+        BTNMult.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    double num1 = Integer.parseInt(TFNum1.getText());
+                    double num2 = Integer.parseInt(TXNum2.getText());
+
+                    JOptionPane.showInputDialog(null, "La Respuesta es: "+ (num1*num2));
+
+
+                }
+        });
+        BTNDiv.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    double num1 = Integer.parseInt(TFNum1.getText());
+                    double num2 = Integer.parseInt(TXNum2.getText());
+
+                    JOptionPane.showInputDialog(null, "La Respuesta es: "+ (num1/num2));
+
+
+                }
+        });
+        borrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TFNum1.setText("");
+                TXNum2.setText("");
             }
         });
     }
 
 
     public static void main(String[] args) {
+        Form1 form = new Form1();
 
 
     }
